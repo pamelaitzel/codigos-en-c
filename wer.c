fc012106b9b12864b1434f00b3edc32d5b4ee524
pame <135767172+pamelaitzel@users.noreply.github.com>
2024-02-24 13:05:35 -0600

    Update wer.c

--- wer.c
+++ wer.c
@@ -1,23 +1,37 @@
 
 
-int main() {
-    int n;
-    double suma = 0;
-
-    printf("Ingrese la cantidad de números: ");
-    scanf("%d", &n);
-
-    printf("Ingrese los números:\n");
-    for (int i = 0; i < n; ++i) {
-        double num;
-        scanf("%lf", &num);
-        suma += num;
+int esPrimo(int num) {
+    if (num < 2) {
+        return 0; // No es primo
     }
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return 0; // No es primo
+        }
+    }
+    return 1; // Es primo
+}
 
-    double promedio = suma / n;
-    printf("El promedio de los números es: %.2lf\n", promedio);
+void generarPrimosEnRango(int inicio, int fin) {
+    printf("Números primos en el rango de %d a %d:\n", inicio, fin);
+    for (int i = inicio; i <= fin; ++i) {
+        if (esPrimo(i)) {
+            printf("%d, ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int rangoInicio, rangoFin;
+
+    printf("Ingrese el rango de números (inicio fin): ");
+    scanf("%d %d", &rangoInicio, &rangoFin);
+
+    generarPrimosEnRango(rangoInicio, rangoFin);
 
     return 0;
 }
+
 
 
